eaba54dead10e413c2216a2f0277304d02caceeb
cfking24 <77686459+cfking24@users.noreply.github.com>
2024-03-09 22:05:29 -0800

    implemented 2 Opt Move

---
+++ src/greed2OptMove.py
@@ -0,0 +1,31 @@
+from src.tspInstance import tspInstance
+
+class Greedy2OptMove:
+    def __init__(self, tsp_instance):
+        self.tsp_instance = tsp_instance
+
+    def run(self, initial_solution):
+        # Copy the initial solution
+        solution = initial_solution.copy()
+
+        # Initialize a variable to keep track of improvements
+        improvement = True
+
+        # While the solution is improving
+        while improvement:
+            # Assume no improvement will be found in this iteration
+            improvement = False
+
+            # Iterate over pairs of indices (i, j) with i < j
+            for i in range(len(solution) - 1):
+                for j in range(i + 2, len(solution) + (i != 0)):
+                    # Calculate the change in distance if we reverse the subpath from i to j
+                    old_distance = self.tsp_instance.graph[solution[i - 1]][solution[i]] + self.tsp_instance.graph[solution[j - 1]][solution[j % len(solution)]]
+                    new_distance = self.tsp_instance.graph[solution[i - 1]][solution[j - 1]] + self.tsp_instance.graph[solution[i]][solution[j % len(solution)]]
+
+                    # If the change in distance is negative, reverse the subpath from i to j
+                    if new_distance < old_distance:
+                        solution[i:j] = reversed(solution[i:j])
+                        improvement = True
+
+        return solution
